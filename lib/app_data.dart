6731bcaab183967a4756201468d605fac190dd8f
“Tunde <contact@tundealao.com>
2024-03-25 11:12:59 +0000

    Add colors

--- lib/app_data.dart
+++ lib/app_data.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = Color(0xFF111111);
+class AppColors {
+  static final Color primary = Color(0xFF5DD600);
+  static final Color secondary = Color(0xFFCEF3B3);
+  static final Color grayText = Color(0xFFE5E5E5);
+}
